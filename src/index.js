6d29af447b4bcb8b7b9ecb83b7f8416d1722bd90
Anesiwa <89821512+Anesiwa@users.noreply.github.com>
2023-01-02 16:18:33 -0600

    Trying to add a basic React route to clothing store

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const container = document.getElementById("root");
-const root = createRoot(container); // createRoot(container!) if you use TypeScript
-root.render(<App tab="home" />);
+const root = createBrowserRouter([[{ path: "/", element: <App /> }]]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={root} />,
+  root
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
